2ed985fcccb88a431fb21dab28e7cd8cb92b9d97
chiri <shenchiri576@gmail.com>
2025-02-07 20:37:54 +0800

    dashboard apperance changed

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,6 +1,38 @@
 return {
-	"goolord/alpha-nvim",
-	config = function()
-		require("alpha").setup(require("alpha.themes.dashboard").config)
-	end,
+    "goolord/alpha-nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+
+        dashboard.section.header.val = {
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                     ]],
+            [[       ████ ██████           █████      ██                     ]],
+            [[      ███████████             █████                             ]],
+            [[      █████████ ███████████████████ ███   ███████████   ]],
+            [[     █████████  ███    █████████████ █████ ██████████████   ]],
+            [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+            [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+            [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+        }
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+            dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
+            dashboard.button("s", "  Settings", ":e $MYVIMRC<CR>"),
+            dashboard.button("q", "󰈆  Quit NVIM", ":qa<CR>"),
+        }
+
+        alpha.setup(dashboard.opts)
+    end,
 }
