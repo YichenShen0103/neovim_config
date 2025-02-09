21c1d6715e1b3f32c3e8734a1fd34bda31812d42
chiri <shenchiri576@gmail.com>
2025-02-09 19:11:58 +0800

    optimize code guide keymap && neotree size

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -32,6 +32,7 @@ return {
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "gb", ":pop<CR>", {})
 
 			-- 自定义诊断符号
 			local signs = {
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,33 +1,36 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
-	config = function()
-		vim.keymap.set("n", "<leader>p", ":Neotree filesystem reveal left toggle<CR>")
-		vim.keymap.set("n", "<leader>b", ":Neotree toggle buffers right<CR>")
-		vim.keymap.set("n", "<leader>s", ":Neotree float git_status<CR>")
-		require("neo-tree").setup({
-			filesystem = {
-				filtered_items = {
-					visible = true, -- 显示过滤后的文件
-				},
-			},
-		})
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+        vim.keymap.set("n", "<leader>p", ":Neotree filesystem reveal left toggle<CR>")
+        vim.keymap.set("n", "<leader>b", ":Neotree toggle buffers right<CR>")
+        vim.keymap.set("n", "<leader>s", ":Neotree float git_status<CR>")
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = true, -- 显示过滤后的文件
+                },
+            },
+            window = {
+                width = 30,
+            },
+        })
 
-		vim.diagnostic.config({
-			signs = {
-				text = {
-					[vim.diagnostic.severity.ERROR] = "",
-					[vim.diagnostic.severity.WARN] = "",
-					[vim.diagnostic.severity.INFO] =  "" ,
-					[vim.diagnostic.severity.HINT] ="󰴓",
-				},
-			},
-		})
-	end,
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = "",
+                    [vim.diagnostic.severity.WARN] = "",
+                    [vim.diagnostic.severity.INFO] = "",
+                    [vim.diagnostic.severity.HINT] = "󰴓",
+                },
+            },
+        })
+    end,
 }
