380350807cacc43f25f8ebfba0a64d7fc60e8962
ycshenMac <shenchiri576@gmail.com>
2025-03-21 13:02:30 +0800

    live-server eager load

--- lua/plugins/live-server.lua
+++ lua/plugins/live-server.lua
@@ -1,9 +1,10 @@
+-- requirement: live-server
+-- run `sudo npm install -g live-server`
+
 return {
-	"barrett-ruth/live-server.nvim",
-	cmd = { "LiveServerStart", "LiveServerStop" },
-	config = function()
-		require("live-server").setup()
-		vim.keymap.set("n", "<leader>o", ":LiveServerStart<CR>", { noremap = true, silent = false })
-		vim.keymap.set("n", "<leader>c", ":LiveServerStop<CR>", { noremap = true, silent = false })
-	end,
+    "barrett-ruth/live-server.nvim",
+    config = function()
+        require("live-server").setup()
+        vim.keymap.set("n", "<leader>L", ":LiveServerToggle<CR>", {}) -- host establish
+    end,
 }
