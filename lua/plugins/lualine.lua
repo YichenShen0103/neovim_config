8606407cbae67733d3597c0dc4b8425e9ce506b5
ycshenMac <shenchiri576@gmail.com>
2025-05-21 23:17:39 +0800

    add remote and noice

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,30 +1,41 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local auto = require("lualine.themes.auto")
-        auto.normal.c.bg = "#ffff"
-        auto.inactive.c.bg = "#ffff"
-        require("lualine").setup({
-            options = {
-                theme = auto,
-            },
-            sections = {
-                lualine_a = { "mode" },
-                lualine_b = { "branch", "diff", "diagnostics" },
-                lualine_c = { "filename" },
-                lualine_x = { "encoding", "filetype" },
-                lualine_y = { "progress" },
-                lualine_z = { "location" },
-            },
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = { "filename" },
-                lualine_x = { "location" },
-                lualine_y = {},
-                lualine_z = {},
-            },
-        })
-    end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local auto = require("lualine.themes.auto")
+		auto.normal.c.bg = "#ffff"
+		auto.inactive.c.bg = "#ffff"
+		require("lualine").setup({
+			options = {
+				theme = auto,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {
+					{
+						function()
+							return vim.g.remote_neovim_host and ("Remote: %s"):format(vim.uv.os_gethostname()) or ""
+						end,
+						padding = { right = 1, left = 1 },
+						separator = { left = "", right = "" },
+					},
+					"branch",
+					"diff",
+					"diagnostics",
+				},
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+		})
+	end,
 }
