eb1cb1e8f8c8d172a6b03eedd5a530a7931bbd1e
ycshenMac <shenchiri576@gmail.com>
2025-05-25 03:20:24 +0800

    Copliot chat plugin added

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,25 @@
 return {
-	"github/copilot.vim",
+    {
+        "github/copilot.vim",
+    },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "github/copilot.vim" },              -- or zbirenbaum/copilot.lua
+            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+        },
+        build = "make tiktoken",                   -- Only on MacOS or Linux
+        opts = {
+            -- See Configuration section for options
+        },
+        -- See Commands section for default commands if you want to lazy load on them
+        config = function()
+            require("CopilotChat").setup({
+                window = {
+                    width = 0.4,
+                },
+            })
+            vim.keymap.set("n", "<leader>cc", ":CopilotChatToggle<CR>", { desc = "Copilot Chat" })
+        end,
+    },
 }
